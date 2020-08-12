ba9ef4023e68a769f8b0d580c7fc7ba96d7f0f0d
ohsuyuki <papu8osu@gmail.com>
2020-08-12 13:27:12 +0900

    テキスト検出を追加

---
+++ text_detection.rb
@@ -0,0 +1,16 @@
+image_path = './resources/sample_license.jpg'
+
+require "google/cloud/vision"
+
+image_annotator = Google::Cloud::Vision.image_annotator
+
+response = image_annotator.text_detection(
+  image:       image_path,
+  max_results: 1 # optional, defaults to 10
+)
+
+response.responses.each do |res|
+  res.text_annotations.each do |text|
+    puts text.description
+  end
+end
